ddd58e42db638d97dda6256a3b83ab10943ca80d
dheemanthpapolu <dheemanth9805@gmail.com>
2025-07-01 16:58:43 +0530

    Create constants.js

---
+++ data/constants.js
@@ -0,0 +1,6 @@
+export const API_KEY = "ab28a3ba5646eab8c00adbaf46c9e39a";
+
+// Base URLs
+export const WEATHER_BASE_URL = "https://api.openweathermap.org/data/2.5/weather";
+export const FORECAST_BASE_URL = "https://api.openweathermap.org/data/2.5/forecast";
+export const AIR_POLLUTION_URL = "https://api.openweathermap.org/data/2.5/air_pollution";
